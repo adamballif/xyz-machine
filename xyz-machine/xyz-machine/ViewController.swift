4ef78f642198469d4b10f7f234143df623e9e25c
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-02-05 21:09:33 -0700

    created xcode project

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Adam Ballif on 2/5/19.
+//  Copyright © 2019 Adam Ballif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
